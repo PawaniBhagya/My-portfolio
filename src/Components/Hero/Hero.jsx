4f79aa36e82f8d6be3d9144f2090191208fe4c17
Pawani Bhagya <119883844+PawaniBhagya@users.noreply.github.com>
2025-03-16 22:18:12 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <div className="hero-resume">
-              <a href="/PawaniBhagya.pdf" onClick={handleResumeClick}>My Resume</a></div>
+              <a href="/PawaniBhagya.pdf" onClick={handleResumeClick} style={{color:"white", textDecoration:"none"}}>My Resume</a></div>
         </div>
     </div>
   )
